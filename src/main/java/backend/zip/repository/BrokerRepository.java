5905d35dcc2728649b50ad33c8ecbf962d8b9033
chahyunsoo <chahs1012@naver.com>
2024-01-21 03:52:48 +0900

    [fix] : application.yml 파일 삭제
    .gitignore 파일에 추가 하지 않았던 application.yml 추가

--- src/main/java/backend/zip/repository/BrokerRepository.java
+++
@@ -1,7 +0,0 @@
-//package backend.zip.repository;
-//
-//import backend.zip.domain.broker.Broker;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface BrokerRepository extends JpaRepository<Broker,Long> {
-//}
--- src/main/java/backend/zip/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-//package backend.zip.repository;
-//
-//import backend.zip.domain.user.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User,Long> {
-//
-//}
