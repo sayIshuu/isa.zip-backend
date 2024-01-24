9e203a6099dab50828dd6910ac059e047cc66c92
chahyunsoo <chahs1012@naver.com>
2024-01-24 15:20:23 +0900

    [feat] : 성공 상태를 정의하는 열거형 추가

---
+++ src/main/java/backend/zip/global/status/SuccessStatus.java
@@ -0,0 +1,36 @@
+package backend.zip.global.status;
+
+import backend.zip.global.code.BaseCode;
+import backend.zip.global.dto.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+    _OK(HttpStatus.OK, "200", "성공 입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
