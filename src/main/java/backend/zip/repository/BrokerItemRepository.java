8639b94630a66c90321ee3582e02c551dd43a60e
chahyunsoo <chahs1012@naver.com>
2024-01-24 17:48:11 +0900

    [fix] : BrokerItem 기본 세팅 코드 수정
    Repository 메소드 명 변경 및 확인용 코드 구현

--- src/main/java/backend/zip/repository/BrokerItemRepository.java
+++ src/main/java/backend/zip/repository/BrokerItemRepository.java
@@ -1,25 +1,16 @@
 package backend.zip.repository;
 
-import backend.zip.domain.broker.Broker;
 import backend.zip.domain.broker.BrokerItem;
-import backend.zip.domain.broker.BrokerOption;
-import backend.zip.domain.enums.ItemStatus;
+import backend.zip.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BrokerItemRepository extends JpaRepository<BrokerItem, Long> {
     //UserID(BrokerID)에 따른 매물 조회
-    List<BrokerItem> findAllByUserId(Long userId);
+    List<BrokerItem> findAllByUser(User user);
 
-    //매물 상세 조회
-    Optional<BrokerItem> findBrokerItemByBrokerItemId(Long brokerItemId);
-
-    //지역에 따른 요청 조회 -> '동' 처리를 다시 해야함
-    List<BrokerItem> findBrokerItemByAddress(String address);
+//    //지역에 따른 요청 조회 -> '동' 처리를 다시 해야함
+//    List<BrokerItem> findBrokerItemByAddress(String address);
 
 }
