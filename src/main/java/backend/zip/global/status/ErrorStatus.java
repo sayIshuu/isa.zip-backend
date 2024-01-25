5fa04012a22400705c6f453997f6ae8d5daa96bc
chahyunsoo <chahs1012@naver.com>
2024-01-25 18:01:26 +0900

    [refactor] : ErrorStatus 에러 상태 변경

--- src/main/java/backend/zip/global/status/ErrorStatus.java
+++ src/main/java/backend/zip/global/status/ErrorStatus.java
@@ -10,13 +10,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
     //기본(전역) 에러
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"500", "서버에서 요청을 처리 하는 동안 오류가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST,"400", "입력 값이 잘못된 요청 입니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"401", "인증이 필요 합니다."),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "403", "금지된 요청 입니다."),
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON_500", "서버에서 요청을 처리 하는 동안 오류가 발생했습니다."),
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON_400", "입력 값이 잘못된 요청 입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON_401", "인증이 필요 합니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_403", "금지된 요청 입니다."),
 
     //User 관련 에러
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "존재 하지 않는 사용자 입니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "존재 하지 않는 사용자 입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
