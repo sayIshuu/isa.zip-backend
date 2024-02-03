89298f7c8be0e7f89091ac707870db6d93dbdd5c
chahyunsoo <chahs1012@naver.com>
2024-02-04 05:49:28 +0900

    [refactor] : 변경된 주소 데이터 맞춰서 코드 정리
    양방향 말고 단뱡항으로 해결해보자 한번

--- src/main/java/backend/zip/dto/brokeritem/response/BrokerItemResponse.java
+++ src/main/java/backend/zip/dto/brokeritem/response/BrokerItemResponse.java
@@ -1,12 +1,18 @@
 package backend.zip.dto.brokeritem.response;
 
+import backend.zip.domain.broker.BrokerItem;
+import backend.zip.domain.broker.BrokerOption;
+import backend.zip.domain.item.ItemContent;
+import backend.zip.domain.item.ItemImage;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
 public class BrokerItemResponse {
     private BrokerItemAddressResponse addressResponse;
     private BrokerItemDetailResponse detailResponse;
@@ -18,5 +24,21 @@ public class BrokerItemResponse {
         this.addressResponse = addressResponse;
         this.detailResponse = detailResponse;
         this.optionResponse = optionResponse;
+    }
+
+    public static BrokerItemResponse getBrokerItemResponse(BrokerItem savedBrokerItem, BrokerItemAddressResponse addressResponse) {
+        // BrokerDetailResponse 생성
+        List<ItemImage> itemImages = savedBrokerItem.getItemImages();
+        ItemContent itemContent = savedBrokerItem.getItemContent();
+        BrokerItemDetailResponse detailResponse = new BrokerItemDetailResponse(itemImages, itemContent);
+
+        // BrokerOptionResponse 생성
+        BrokerOption brokerOption = savedBrokerItem.getBrokerOption();
+        BrokerItemOptionResponse optionResponse = new BrokerItemOptionResponse(brokerOption);
+
+        // BrokerItemResponse 생성
+        BrokerItemResponse itemResponse = new BrokerItemResponse(addressResponse, detailResponse, optionResponse);
+
+        return itemResponse;
     }
 }
--- src/main/java/backend/zip/service/broker/BrokerItemSaveService.java
+++ src/main/java/backend/zip/service/broker/BrokerItemSaveService.java
@@ -3,9 +3,10 @@ package backend.zip.service.broker;
 import backend.zip.domain.broker.BrokerItem;
 import backend.zip.dto.brokeritem.request.AddBrokerItemDetailsRequest;
 import backend.zip.dto.brokeritem.request.AddBrokerItemOptionsRequest;
+import backend.zip.dto.brokeritem.response.BrokerItemAddressResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface BrokerItemSaveService {
-    public BrokerItem saveBrokerItem(Long userId, BrokerItem brokerItem, String address, String dong, Double x, Double y,
+    public BrokerItem saveBrokerItem(Long userId, BrokerItem brokerItem, BrokerItemAddressResponse brokerItemAddressResponse,
                                      AddBrokerItemDetailsRequest detailsRequest, MultipartFile[] brokerItemImg, AddBrokerItemOptionsRequest optionsRequest);
 }
