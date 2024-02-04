e118d27132a79b392b031dd499221f53af7896bf
chahyunsoo <chahs1012@naver.com>
2024-02-05 06:40:59 +0900

    [feat] : 매물 아이템 수정 기능 구현

--- src/main/java/backend/zip/service/broker/BrokerItemService.java
+++ src/main/java/backend/zip/service/broker/BrokerItemService.java
@@ -4,7 +4,10 @@ import backend.zip.domain.broker.BrokerItem;
 import backend.zip.dto.brokeritem.request.AddBrokerItemDetailsRequest;
 import backend.zip.dto.brokeritem.request.AddBrokerItemOptionsRequest;
 import backend.zip.dto.brokeritem.response.BrokerItemAddressResponse;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface BrokerItemService {
     public BrokerItem saveBrokerItem(Long userId, BrokerItem brokerItem, BrokerItemAddressResponse brokerItemAddressResponse,
@@ -12,4 +15,12 @@ public interface BrokerItemService {
 
     public void deleteBrokerItem(Long brokerItemId);
 
+//    public BrokerItem findBrokerItem(Long brokerItemId);
+
+//    public List<BrokerItem> findAllBrokerItem(Long userId);
+
+    @Transactional
+    public BrokerItem updateBrokerItem(Long brokerItemId, String roadFullAddress,
+                                AddBrokerItemDetailsRequest detailsRequest,
+                                AddBrokerItemOptionsRequest optionsRequest, MultipartFile[] multipartFiles);
 }
