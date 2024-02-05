c34e604ef1d9062f762237f976416108e9afc368
chahyunsoo <chahs1012@naver.com>
2024-02-05 23:53:03 +0900

    [feat] : 매물 전체 조회 및 매물 단건 조회 구현

--- src/main/java/backend/zip/repository/broker/BrokerItemRepository.java
+++ src/main/java/backend/zip/repository/broker/BrokerItemRepository.java
@@ -11,14 +11,24 @@ import java.util.Optional;
 
 public interface BrokerItemRepository extends JpaRepository<BrokerItem, Long> {
     //UserID(BrokerID)에 따른 매물 조회
-    List<BrokerItem> findAllByUser(User user);
+//    List<BrokerItem> findAllByUserId(Long userId);
 
-    //동에 따른 전체 매물 요청 조회
-    List<BrokerItem> findByDong(String dong);
+//    //동에 따른 전체 매물 요청 조회
+//    List<BrokerItem> findByDong(String dong);
 
     Optional<BrokerItem> findById(Long brokerItemId);
 
-    @Query("SELECT b FROM BrokerItem b WHERE b.user.id = :userId AND b.id = :brokerItemId")
-    Optional<BrokerItem> findBrokerItemByUserIdAndBrokerItemId(@Param("userId") Long userId, @Param("brokerItemId") Long brokerItemId);
+//    @Query("SELECT b FROM BrokerItem b WHERE b.user.id = :userId AND b.id = :brokerItemId")
+//    Optional<BrokerItem> findBrokerItemByUserIdAndBrokerItemId(@Param("userId") Long userId, @Param("brokerItemId") Long brokerItemId);
+
+//    List<BrokerItem> findBrokerItemByUser(Long userId);
+
+    @Query("select b from BrokerItem b where b.user.id = :userId")
+    List<BrokerItem> findBrokerItemByUser(@Param("userId") Long userId);
+
+
+//    @Query("select b from BrokerItem b where b.user.id = :userId")
+//    List<BrokerItem> findBrokerItemByUser(@Param("userId") Long userId);
+
 
 }
