d002bea9b068af9f5918d8bc68b0b00b3aec4866
326eunjin <326eunjin@naver.com>
2024-02-07 22:23:03 +0900

    [Refactor] : ScheduleServiceImpl 수정
    -스케줄 등록시, 이벤트들도 자동생성되게 함.
    
    Fixes : #34

--- src/main/java/backend/zip/service/schedule/ScheduleServiceImpl.java
+++ src/main/java/backend/zip/service/schedule/ScheduleServiceImpl.java
@@ -1,14 +1,17 @@
 package backend.zip.service.schedule;
 
+import backend.zip.domain.enums.Period;
 import backend.zip.domain.schedule.Schedule;
 import backend.zip.domain.user.User;
 import backend.zip.dto.schedule.request.AddScheduleRequest;
 import backend.zip.dto.schedule.request.UpdateScheduleRequest;
-import backend.zip.repository.ScheduleRepository;
+import backend.zip.Repository.schedule.ScheduleRepository;
 import backend.zip.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
@@ -17,6 +20,8 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
     private final UserRepository userRepository;
+    @Autowired
+    private EventService eventService;
 
     @Override
     public Optional<Schedule> getScheduleByUserId(Long userId) {
@@ -29,6 +34,7 @@ public class ScheduleServiceImpl implements ScheduleService {
         User user = userRepository.findById(userId).orElse(null);
         if (user != null) {
             Schedule newSchedule = new Schedule(null, user, request.getPeriod(), request.getMoveDate());
+            generateEventsForSchedule(newSchedule, request);
             return scheduleRepository.save(newSchedule);
         }
 
@@ -52,4 +58,50 @@ public class ScheduleServiceImpl implements ScheduleService {
             scheduleRepository.deleteByUser(deleteUser);
         }
     }
+
+
+    private void generateEventsForSchedule(Schedule schedule, AddScheduleRequest request) {
+        LocalDate moveDate = request.getMoveDate();
+        Period movePeriod = schedule.getPeriod();
+        switch (movePeriod) {
+            case THREE_MONTHS:
+                eventService.addEvent(schedule,moveDate.minusDays(90),"집주인에게 연락");
+                eventService.addEvent(schedule,moveDate.minusDays(70),"이사 날짜 정하기, 예산 설정");
+                eventService.addEvent(schedule,moveDate.minusDays(60),"공인중개사 컨택");
+                eventService.addEvent(schedule,moveDate.minusDays(55),"방 확정");
+                eventService.addEvent(schedule,moveDate.minusDays(50),"계약금 입금 및 이사 날짜 확정 논의");
+                eventService.addEvent(schedule,moveDate.minusDays(40),"계약서 작성");
+                eventService.addEvent(schedule,moveDate.minusDays(7),"짐 싸기, 전입 신고, 이사 업체 선정");
+                eventService.addEvent(schedule,moveDate.minusDays(0),"가스, 전기, 수도요금 검침 및 정산");
+                eventService.addEvent(schedule,moveDate.minusDays(0),"집 옮기기, 정돈");
+                break;
+            case TWO_MONTHS:
+                eventService.addEvent(schedule,moveDate.minusDays(60),"집주인에게 연락");
+                eventService.addEvent(schedule,moveDate.minusDays(45),"이사 날짜 정하기, 예산 설정");
+                eventService.addEvent(schedule,moveDate.minusDays(40),"공인중개사 컨택");
+                eventService.addEvent(schedule,moveDate.minusDays(35),"방 확정");
+                eventService.addEvent(schedule,moveDate.minusDays(33),"계약금 입금 및 이사 날짜 확정 논의");
+                eventService.addEvent(schedule,moveDate.minusDays(25),"계약서 작성");
+                eventService.addEvent(schedule,moveDate.minusDays(7),"짐 싸기, 전입 신고, 이사 업체 선정");
+                eventService.addEvent(schedule,moveDate.minusDays(0),"가스, 전기, 수도요금 검침 및 정산");
+                eventService.addEvent(schedule,moveDate.minusDays(0),"집 옮기기, 정돈");
+                break;
+            case ONE_MONTH:
+                eventService.addEvent(schedule,moveDate.minusDays(30),"이사 날짜 정하기, 예산 설정");
+                eventService.addEvent(schedule,moveDate.minusDays(26),"공인중개사 컨택");
+                eventService.addEvent(schedule,moveDate.minusDays(23),"방 확정");
+                eventService.addEvent(schedule,moveDate.minusDays(20),"계약금 입금 및 이사 날짜 확정 논의");
+                eventService.addEvent(schedule,moveDate.minusDays(15),"계약서 작성");
+                eventService.addEvent(schedule,moveDate.minusDays(7),"짐 싸기, 전입 신고, 이사 업체 선정");
+                eventService.addEvent(schedule,moveDate.minusDays(0),"가스, 전기, 수도요금 검침 및 정산");
+                eventService.addEvent(schedule,moveDate.minusDays(0),"집 옮기기, 정돈");
+                break;
+            case TWO_WEEKS:
+
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid period: " + schedule.getPeriod()); // 에러 처리 추후 수정
+        }
+    }
+
 }
