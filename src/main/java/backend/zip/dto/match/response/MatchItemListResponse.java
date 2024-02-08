000e0e1dd9515a250da96e46c498974173715f69
chahyunsoo <chahs1012@naver.com>
2024-02-09 05:31:03 +0900

    [feat] : 매물전체조회 할 때 반환 데이터, 매물완료상태 시 반환 데이터 정의

---
+++ src/main/java/backend/zip/dto/match/response/MatchItemListResponse.java
@@ -0,0 +1,37 @@
+package backend.zip.dto.match.response;
+
+import backend.zip.domain.match.Matching;
+import backend.zip.dto.brokeritem.response.BrokerItemResponse;
+import backend.zip.dto.useritem.response.UserItemResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MatchItemListResponse {
+    private List<MatchListDetails> matchListDetails;
+
+    @Getter
+    @AllArgsConstructor
+    public static class MatchListDetails {
+        private Long matchId;
+        private UserItemResponse userItemResponse;
+        private BrokerItemResponse brokerItemResponse;
+    }
+
+    public static MatchItemListResponse of(List<Matching> matchingList) {
+        List<MatchListDetails> matchListDetailsList = matchingList.stream()
+                .map(matching -> new MatchListDetails(
+                        matching.getMatchID(),
+                        UserItemResponse.from(matching.getUserItem()),
+                        BrokerItemResponse.getBrokerItemResponse(matching.getBrokerItem())
+                )).collect(Collectors.toList());
+
+        return new MatchItemListResponse(matchListDetailsList);
+    }
+}
