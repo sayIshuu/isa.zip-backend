aebc2c08e1e8639598344cc27700e71b5a3649b6
sayishuu <smile52673@gmail.com>
2024-02-14 02:59:38 +0900

    [refactor] : 유저사이드매칭조회 FetchJoin 적용

--- src/main/java/backend/zip/repository/MatchRepository.java
+++ src/main/java/backend/zip/repository/MatchRepository.java
@@ -12,9 +12,13 @@ import java.util.List;
 @Repository
 public interface MatchRepository extends JpaRepository<Matching, Long> {
 
-    // userId가 같은 userItem을 가지고 있고 matchStatus가 같은 Matching 객체들을 반환
+    // Fetch Join이용해서 userId가 같은 userItem을 가지고 있고 matchStatus가 같은 Matching 객체들을 반환
     // 양방향 참조 상태라 이런 쿼리문 가능
-    @Query("select m from Matching m where m.userItem.user.id = :userId and m.matchStatus = :matchStatus")
+    //@Query("select m from Matching m where m.userItem.user.id = :userId and m.matchStatus = :matchStatus")
+    @Query("select m from Matching m join fetch m.userItem ui join fetch ui.userOption uo " +
+            "join fetch m.brokerItem bi join fetch bi.itemContent join fetch bi.itemImages join fetch bi.brokerOption " +
+            "join fetch bi.user u join fetch u.broker ub " +
+            "where m.userItem.user.id = :userId and m.matchStatus = :matchStatus")
     List<Matching> findByUserItemUserIdAndMatchStatus(Long userId, MatchStatus matchStatus);
 
 
