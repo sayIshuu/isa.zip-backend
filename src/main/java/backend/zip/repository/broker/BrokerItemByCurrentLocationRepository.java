2e23d701e7a5ae4f1f61411dcb3b769ec2ed8c84
chahyunsoo <chahs1012@naver.com>
2024-02-15 04:04:18 +0900

    [feat] : 매물 탭에서 현재 위치에서 반경 2km 내 매물 아이템 반환 api 구현 완료

--- src/main/java/backend/zip/repository/broker/BrokerItemByCurrentLocationRepository.java
+++ src/main/java/backend/zip/repository/broker/BrokerItemByCurrentLocationRepository.java
@@ -8,11 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BrokerItemByCurrentLocationRepository extends JpaRepository<BrokerItem, Long> {
-//    @Query(value = "select * from BrokerItem b " +
-////            "join fetch b.brokerOption bo " +
-////            "join fetch b.itemContent " +
-////            "join fetch b.itemImages " +
-//            "where ST_Distance_Sphere(point(:lng, :lat), point(x, y)) <= 2000", nativeQuery = true)
-
-    List<BrokerItem> findBrokerItemsWithinRadius(@Param("lat") Double lat, @Param("lng") Double lng);
+    @Query(value = "select * from broker_item where ST_Distance_Sphere(point(:lng,:lat),point(x,y)) <= 2000",nativeQuery = true)
+    List<BrokerItem> findBrokerItemsWithinRadius(@Param("lng") Double lng, @Param("lat") Double lat);
 }
